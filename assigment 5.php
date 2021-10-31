531f5e6f34d7160b81291e1c477a0371821ea277
kawsar69598 <92175782+kawsar69598@users.noreply.github.com>
2021-10-31 10:40:52 +0600

    Add files via upload

---
+++ assigment 5.php
@@ -0,0 +1,19 @@
+<!doctype html >
+<head>
+      <title>
+    calculator(9th class)
+</title>
+</head>
+ <?php 
+ 
+ 
+ 
+ 
+ ?>
+<body>
+    <h1>my calculator</h1>
+    <form method="post">
+        <label>
+            fast matrix
+</label>
+<input type= "number" >
